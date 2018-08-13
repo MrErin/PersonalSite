12549af54a6b375158383a2988bce0119736dfbd
MrErin <erinmeaker@gmail.com>
2018-08-13 16:59:34 -0500

    working

--- src/scripts/backgroundChange.js
+++ src/scripts/backgroundChange.js
@@ -4,21 +4,21 @@ const classPicker = (header) => {
 	// This function returns which image should be shown depending on which anchor is passed in
 	switch (header) {
 	case 'ProjectsA':
-		return './assets/img/bulb1.png'
+		return 'one'
 		break
 	case 'SkillsA':
-		return './assets/img/bulb2.png'
+		return 'two'
 		break
 	case 'ContactA':
-		return './assets/img/bulb3.png'
+		return 'three'
 		break
 	case 'HomeA':
 	case 'BioA':
-		return './assets/img/off.png'
+		return 'off'
 		break
 	default:
 		console.log(`backgroundChange script attempting to pass ${header} to the add switch statement`)
-		return './assets/img/off.png'
+		return 'off'
 		break
 	}
 }
@@ -36,9 +36,7 @@ $.fn.viewCheck = function() {
 
 
 const backgroundChange = () => {
-	let scrollTimeout
-	const throttle = 500
-	let lastScrollTop = 0
+	let oldAnchorId = 'HomeA'
 
 	//preloads images
 	let bulbs = []
@@ -49,45 +47,66 @@ const backgroundChange = () => {
 	})
 
 	$(window).on('resize scroll', function() {
-		let st = $(this).scrollTop()
-		if (!scrollTimeout) {
-			scrollTimeout = setTimeout(function() {
-				$('.anchor').each(function() {
-					let activeAnchor = $(this).attr('id')
-					if($(this).viewCheck()){
-						// $('#bulb').fadeOut(function(){
-						// 	$(this).on('load', (function() { $(this).fadeIn()}))
-						// 	$(this).attr('src', classPicker(activeAnchor))
-						// })
+		let oldImg = $('#bulbDiv .changeThis')
+		$('.anchor').each(function() {
+			let activeAnchor = $(this)
+			let activeAnchorId = $(this).attr('id')
+			let oldImg = $('#bulbDiv .changeThis')
+			let img = new Image()
+			img.src = $(document.getElementById(classPicker(activeAnchorId))).attr('src')
+			let newImg = $(img).hide()
+			console.log(`activeAnchorID = ${activeAnchorId}, oldImg = ${oldImg.attr('id')}, img = ${img}, newImg = ${newImg}`)
+			if($(this).viewCheck()){
+				if ($(this).attr('id') !== oldAnchorId) {
+					// console.log(`anchor was ${oldAnchorId}`)
+					// oldAnchorId = $(this).attr('id')
+					// console.log(`anchor is now ${oldAnchorId}`)
+					// let newImgEl = document.getElementById(classPicker(oldAnchorId))
+					// newImg.src = $(newImgEl).attr('src')
+					$('#bulbDiv').append(img)
+					oldImg.stop(true).fadeOut(500, function() {
+						$(this).remove()
+					})
+					newImg.fadeIn(500).addClass('bulbImg changeThis')
+					return false
+					// console.log($(newImg).attr('src'))
 
-						// $('#bulb').attr('src', classPicker(activeAnchor)).fadeIn(2000)
-						// if (st > lastScrollTop){
-						// 	//down
-						// 	console.log(`down; st = ${st} lastscrolltop = ${lastScrollTop}`)
-						// } else {
-						// 	//up
-						// 	console.log(`up; st = ${st} lastscrolltop = ${lastScrollTop}`)
-						// }
-						// lastScrollTop = st
 
-						let oldImg = $('#bulbDiv .changeThis')
-						let img = new Image()
-						img.src = classPicker(activeAnchor)
-						let newImg = $(img).hide()
-						$('#bulbDiv').append(img)
 
-						oldImg.stop(true).fadeOut(500, function() {
-							$(this).remove()
-						})
-						newImg.fadeIn(500).addClass('bulbImg changeThis')
-						return false
-					}
-				})
-				scrollTimeout = null
-			}, throttle)
-		}
+
+				}
+				// $('#bulb').fadeOut(function(){
+				// 	$(this).on('load', (function() { $(this).fadeIn()}))
+				// 	$(this).attr('src', classPicker(activeAnchor))
+				// })
+
+				// $('#bulb').attr('src', classPicker(activeAnchor)).fadeIn(2000)
+				// if (st > lastScrollTop){
+				// 	//down
+				// 	console.log(`down; st = ${st} lastscrolltop = ${lastScrollTop}`)
+				// } else {
+				// 	//up
+				// 	console.log(`up; st = ${st} lastscrolltop = ${lastScrollTop}`)
+				// }
+				// lastScrollTop = st
+
+
+				// let oldImg = $('#bulbDiv .changeThis')
+				// let img = new Image()
+				// img.src = classPicker(activeAnchor)
+				// let newImg = $(img).hide()
+				// $('#bulbDiv').append(img)
+
+				// oldImg.stop(true).fadeOut(500, function() {
+				// 	$(this).remove()
+				// })
+				// newImg.fadeIn(500).addClass('bulbImg changeThis')
+				// return false
+			}
+		})
 	})
 
 }
+
 
 module.exports = backgroundChange
